7bcef8e39ef2c3dca432bab4844dcf3bbeef5cb9
Filippo Vissani <filippo.vissani@studio.unibo.it>
2024-06-16 09:50:19 +0200

    ci: add vanilla conventional commits

--- release.config.js
+++ release.config.js
@@ -1,10 +1,10 @@
-const config = require('semantic-release-preconfigured-conventional-commits')
 const publishCommands = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md || exit 1
 git push --force origin \${nextRelease.version} || exit 2
 `
-const releaseBranches = ["main"]
-config.branches = releaseBranches
+const config = {
+  branches: ["main"],
+};
 config.plugins.push(
     ["@semantic-release/exec", {
         "publishCmd": publishCommands,
